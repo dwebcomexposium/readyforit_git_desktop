89c8c62b501c3505bf0a014fcf9201bbbc312a48
Flore Chasles <flore.chasles@gmail.com>
2019-04-19 11:20:29 +0200

    Add cookie to change header

--- js/modules/surcharge.js
+++ js/modules/surcharge.js
@@ -1,3 +1,27 @@
+/** Cookie for header **/
+  function setCookie(name, value, days) {
+    var d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
+
+  function getCookie(name) {
+    var cookieName = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(cookieName) == 0) {
+        return c.substring(cookieName.length, c.length);
+      }
+    }
+    return "";
+  }
+
 $(document).ready(function(){
   /** Slick init **/
   $('.videos').slick({
@@ -21,5 +45,13 @@ $(document).ready(function(){
     if (headerHeight > $('#zone1').height()) {
       $('#zone1, .article-title img').css('height', headerHeight);
     }
+  }
+
+  /** Cookie for header **/
+  var cookieName = getCookie("header_cookie");
+  if (cookieName === '') {
+    setCookie('header_cookie', 'header_cookie', 365);
+  } else {
+    $('.site-banner').addClass('is-stuck');
   }
 });
